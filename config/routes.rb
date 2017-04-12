a76d94e9e8cbca430be6c9f7b14d400f58682888
杉山裕磨 <YUMA@sugiyamayuumas-MacBook-Air.local>
2017-04-12 11:36:48 +0900

    generate_users_views_and_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
- 　root 'messages#index'
- 　resources :messages, only: :index
+  root 'messages#index'
+  resources :messages, only: :index
+  resources :users, only: [:show]
 end
