79ab7fb1309c7845118ae103027df5e685d5686f
杉山裕磨 <YUMA@sugiyamayuumas-MacBook-Air.local>
2017-04-13 02:16:49 +0900

    through_path_to_edit_view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :messages, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
 end
