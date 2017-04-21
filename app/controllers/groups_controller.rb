07df1e139922d30aba1776ff540d095b48f137da
杉山裕磨 <YUMA@sugiyamayuumas-MacBook-Air.local>
2017-04-21 18:20:12 +0900

    use_form_for

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,25 @@
 class GroupsController < ApplicationController
 
   def new
-    @user = current_user
     @group = Group.new
+    @users = User.all
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to group_messages_path(@group.id), notice: 'グループが作成されました'
+    else
+      flash.now[:aleat] = 'グループ作成に失敗しました'
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+  def create_params
+    params.require(:group).permit(:name, {user_ids:[]})
   end
 end
