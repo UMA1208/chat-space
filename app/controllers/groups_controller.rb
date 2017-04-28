7a5c3d95aebb9943df90f9bb91cc23185c9ae989
杉山裕磨 <YUMA@sugiyamayuumas-MacBook-Air.local>
2017-04-28 13:17:56 +0900

    set_before_action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
 
+  before_action :set_group, only:[:edit, :update]
+
   def index
     @groups = current_user.groups
   end
@@ -12,15 +14,13 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path(@group.id), alert: 'グループが作成されました'
+      redirect_to messages_path(@group.id), alert: 'グループが作成されました'
     else
       redirect_to root_path(@group.id), alert: 'グループ作成に失敗しました'
     end
   end
 
   def edit
-    @users = User.all
-    set_group
   end
 
   def update
@@ -36,7 +36,7 @@ class GroupsController < ApplicationController
     params.require(:group).permit(:group_name, user_ids:[])
   end
 
-    def set_group
+  def set_group
     @group = Group.find(params[:id])
   end
 
